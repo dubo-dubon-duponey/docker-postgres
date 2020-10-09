35ff196c1f5a0a098fd67c698c366587ae306b61
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2020-10-09 16:17:18 -0700

    Bang
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

---
+++ Dockerfile
@@ -0,0 +1,105 @@
+ARG           BUILDER_BASE=dubodubonduponey/base:builder
+ARG           RUNTIME_BASE=dubodubonduponey/base:runtime
+
+#######################
+# Extra builder for healthchecker
+#######################
+# hadolint ignore=DL3006
+FROM          --platform=$BUILDPLATFORM $BUILDER_BASE                                                                   AS builder-healthcheck
+
+ARG           GIT_REPO=github.com/dubo-dubon-duponey/healthcheckers
+ARG           GIT_VERSION=51ebf8ca3d255e0c846307bf72740f731e6210c3
+
+WORKDIR       $GOPATH/src/$GIT_REPO
+RUN           git clone git://$GIT_REPO .
+RUN           git checkout $GIT_VERSION
+# hadolint ignore=DL4006
+RUN           env GOOS=linux GOARCH="$(printf "%s" "$TARGETPLATFORM" | sed -E 's/^[^/]+\/([^/]+).*/\1/')" go build -v -ldflags "-s -w" \
+                -o /dist/boot/bin/http-health ./cmd/http
+
+#######################
+# Goello
+#######################
+# hadolint ignore=DL3006
+FROM          --platform=$BUILDPLATFORM $BUILDER_BASE                                                                   AS builder-goello
+
+ARG           GIT_REPO=github.com/dubo-dubon-duponey/goello
+ARG           GIT_VERSION=6f6c96ef8161467ab25be45fe3633a093411fcf2
+
+WORKDIR       $GOPATH/src/$GIT_REPO
+RUN           git clone git://$GIT_REPO .
+RUN           git checkout $GIT_VERSION
+# hadolint ignore=DL4006
+RUN           env GOOS=linux GOARCH="$(printf "%s" "$TARGETPLATFORM" | sed -E 's/^[^/]+\/([^/]+).*/\1/')" go build -v -ldflags "-s -w" \
+                -o /dist/boot/bin/goello-server ./cmd/server/main.go
+
+#######################
+# Builder assembly
+#######################
+# hadolint ignore=DL3006
+FROM          $BUILDER_BASE                                                                                             AS builder
+
+COPY          --from=builder-healthcheck /dist/boot/bin /dist/boot/bin
+COPY          --from=builder-goello /dist/boot/bin /dist/boot/bin
+
+RUN           chmod 555 /dist/boot/bin/*; \
+              epoch="$(date --date "$BUILD_CREATED" +%s)"; \
+              find /dist/boot/bin -newermt "@$epoch" -exec touch --no-dereference --date="@$epoch" '{}' +;
+
+#######################
+# Running image
+#######################
+# hadolint ignore=DL3006
+FROM          $RUNTIME_BASE
+
+ARG           PG_MAJOR=13
+ARG           PG_VERSION=13.0-1.pgdg100+1
+
+USER          root
+
+RUN           apt-get update -qq            && \
+              apt-get install -qq --no-install-recommends \
+                curl=7.64.0-4+deb10u1 \
+                gnupg=2.2.12-1+deb10u1      && \
+              curl --proto '=https' --tlsv1.2 -sSfL https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add - && \
+              echo "deb http://apt.postgresql.org/pub/repos/apt buster-pgdg main" | tee /etc/apt/sources.list.d/postgres.list && \
+              apt-get update -qq            && \
+              apt-get install -qq --no-install-recommends \
+                postgresql-common \
+                postgresql-"$PG_MAJOR=$PG_VERSION" && \
+              apt-get purge -qq curl gnupg  && \
+              apt-get -qq autoremove        && \
+              apt-get -qq clean             && \
+              rm -rf /var/lib/apt/lists/*   && \
+              rm -rf /tmp/*                 && \
+              rm -rf /var/tmp/*
+
+USER          dubo-dubon-duponey
+
+COPY          --from=builder --chown=$BUILD_UID:root /dist .
+
+ENV           PATH=/usr/lib/postgresql/$PG_MAJOR/bin/:$PATH
+ENV           PGDATA=/data
+
+STOPSIGNAL    SIGINT
+
+EXPOSE        5432
+VOLUME        /data
+
+# mDNS
+ENV           MDNS_NAME="Fancy Postgres Service Name"
+ENV           MDNS_HOST="postgres"
+ENV           MDNS_TYPE=_postgres._tcp
+
+# Authentication
+ENV           USERNAME="dubo-dubon-duponey"
+ENV           PASSWORD="nhehehehehe"
+ENV           REALM="My precious postgres"
+
+# Log level and port
+ENV           PORT=5432
+
+ENV           HEALTHCHECK_URL=http://127.0.0.1:5432/
+HEALTHCHECK   --interval=30s --timeout=30s --start-period=10s --retries=1 CMD http-health || exit 1
+
+CMD           postgres
---
+++ context/runtime/boot/entrypoint.sh
@@ -0,0 +1,182 @@
+#!/usr/bin/env bash
+# XXX restore
+# set -o errexit -o errtrace -o functrace -o nounset -o pipefail
+
+# Ensure the data folder is writable
+[ -w "/data" ] || {
+  >&2 printf "/data is not writable. Check your mount permissions.\n"
+  exit 1
+}
+
+#if [ "${USERNAME:-}" ]; then
+#  export REGISTRY_AUTH=htpasswd
+#  export REGISTRY_AUTH_HTPASSWD_REALM="$REALM"
+#  export REGISTRY_AUTH_HTPASSWD_PATH=/data/htpasswd
+#  printf "%s:%s\n" "$USERNAME" "$(printf "%s" "$PASSWORD" | base64 -d)" > /data/htpasswd
+#fi
+
+# args=()
+
+# Bonjour the container
+if [ "${MDNS_NAME:-}" ]; then
+  goello-server -name "$MDNS_NAME" -host "$MDNS_HOST" -port "$PORT" -type "$MDNS_TYPE" &
+fi
+
+# Run once configured
+# exec postgres "$@"
+
+
+# XXX
+# Below has been largely copied from postgres official image
+
+POSTGRES_USER="${USERNAME:-}"
+POSTGRES_PASSWORD="${PASSWORD:-}"
+
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+# initialize empty PGDATA directory with new database via 'initdb'
+# arguments to `initdb` can be passed via POSTGRES_INITDB_ARGS or as arguments to this function
+# `initdb` automatically creates the "postgres", "template0", and "template1" dbnames
+# this is also where the database user is created, specified by `POSTGRES_USER` env
+docker_init_database_dir() {
+	eval 'initdb --username="$POSTGRES_USER" --pwfile=<(echo "$POSTGRES_PASSWORD") '"$POSTGRES_INITDB_ARGS"' "$@"'
+}
+
+# Execute sql script, passed via stdin (or -f flag of pqsl)
+# usage: docker_process_sql [psql-cli-args]
+#    ie: docker_process_sql --dbname=mydb <<<'INSERT ...'
+#    ie: docker_process_sql -f my-file.sql
+#    ie: docker_process_sql <my-file.sql
+docker_process_sql() {
+	local query_runner=( psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --no-password )
+	if [ -n "$POSTGRES_DB" ]; then
+		query_runner+=( --dbname "$POSTGRES_DB" )
+	fi
+
+	"${query_runner[@]}" "$@"
+}
+
+# create initial database
+# uses environment variables for input: POSTGRES_DB
+docker_setup_db() {
+	if [ "$POSTGRES_DB" != 'postgres' ]; then
+		POSTGRES_DB= docker_process_sql --dbname postgres --set db="$POSTGRES_DB" <<-'EOSQL'
+			CREATE DATABASE :"db" ;
+		EOSQL
+		echo
+	fi
+}
+
+# Loads various settings that are used elsewhere in the script
+# This should be called before any other functions
+docker_setup_env() {
+	file_env 'POSTGRES_PASSWORD'
+
+	file_env 'POSTGRES_USER' 'postgres'
+	file_env 'POSTGRES_DB' "$POSTGRES_USER"
+	file_env 'POSTGRES_INITDB_ARGS'
+	# default authentication method is md5
+	: "${POSTGRES_HOST_AUTH_METHOD:=md5}"
+
+	declare -g DATABASE_ALREADY_EXISTS
+	# look specifically for PG_VERSION, as it is expected in the DB dir
+	if [ -s "$PGDATA/PG_VERSION" ]; then
+		DATABASE_ALREADY_EXISTS='true'
+	fi
+}
+
+# append POSTGRES_HOST_AUTH_METHOD to pg_hba.conf for "host" connections
+pg_setup_hba_conf() {
+	{
+		echo
+		if [ 'trust' = "$POSTGRES_HOST_AUTH_METHOD" ]; then
+			echo '# warning trust is enabled for all connections'
+			echo '# see https://www.postgresql.org/docs/12/auth-trust.html'
+		fi
+		echo "host all all all $POSTGRES_HOST_AUTH_METHOD"
+	} >> "$PGDATA/pg_hba.conf"
+}
+
+# start socket-only postgresql server for setting up or running scripts
+# all arguments will be passed along as arguments to `postgres` (via pg_ctl)
+docker_temp_server_start() {
+	if [ "$1" = 'postgres' ]; then
+		shift
+	fi
+
+	# internal start of server in order to allow setup using psql client
+	# does not listen on external TCP/IP and waits until start finishes
+	set -- "$@" -c listen_addresses='' -p "${PGPORT:-5432}"
+
+	PGUSER="${PGUSER:-$POSTGRES_USER}" \
+	pg_ctl -D "$PGDATA" \
+		-o "$(printf '%q ' "$@")" \
+		-w start
+}
+
+# stop postgresql server after done setting up user and running scripts
+docker_temp_server_stop() {
+	PGUSER="${PGUSER:-postgres}" \
+	pg_ctl -D "$PGDATA" -m fast -w stop
+}
+
+_main() {
+	# if first arg looks like a flag, assume we want to run postgres server
+	if [ "${1:0:1}" = '-' ]; then
+		set -- postgres "$@"
+	fi
+
+	if [ "$1" = 'postgres' ]; then
+		docker_setup_env
+
+		# only run initialization on an empty data directory
+		if [ -z "$DATABASE_ALREADY_EXISTS" ]; then
+			docker_init_database_dir
+			echo "include = '/config/postgresql.conf'" >> /data/postgresql.conf
+
+			pg_setup_hba_conf
+
+			# PGPASSWORD is required for psql when authentication is required for 'local' connections via pg_hba.conf and is otherwise harmless
+			# e.g. when '--auth=md5' or '--auth-local=md5' is used in POSTGRES_INITDB_ARGS
+			export PGPASSWORD="${PGPASSWORD:-$POSTGRES_PASSWORD}"
+			docker_temp_server_start "$@"
+
+			docker_setup_db
+
+			docker_temp_server_stop
+			unset PGPASSWORD
+
+			echo
+			echo 'PostgreSQL init process complete; ready for start up.'
+			echo
+		else
+			echo
+			echo 'PostgreSQL Database directory appears to contain a database; Skipping initialization'
+			echo
+		fi
+	fi
+
+	exec "$@"
+}
+
+_main "$@"
